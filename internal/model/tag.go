e15334153691c40c7fffbd30efdfe4abe0449129
changjunhang <l>
2021-05-10 16:40:32 +0800

    修改tag swag

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -5,10 +5,17 @@
 
 package model
 
+import "github.com/HuberyChang/blog-service/pkg/app"
+
 type Tag struct {
 	*Model
 	Name  string `json:"name"`
 	State uint8  `json:"state"`
+}
+
+type TagSwagger struct {
+	List  []*Tag
+	Pager *app.Pager
 }
 
 func (t Tag) TableName() string {
